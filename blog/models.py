ab1d132968335e6abfdb2f0c59a58a2da4981c33
hsjusj <905249411@qq.com>
2019-06-30 19:50:35 +0800

    wrote blog.models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Articles(models.Model):
+    aid = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=32, db_index=True)
+    content = models.TextField()
+    create_datetime = models.DateTimeField(auto_now_add=True, db_index=True)
+
+class Tags(models.Model):
+    tid = models.AutoField(primary_key=True)
+    tag_name = models.CharField(max_length=18)
+
+class ArticlesToTags(models.Model):
+    article = models.ForeignKey(to='Articles', to_field='aid', on_delete=models.CASCADE)
+    tag = models.ForeignKey(to='Tags', to_field='tid', on_delete=models.CASCADE)
--- hsjusj/settings.py
+++ hsjusj/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -106,16 +107,19 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
